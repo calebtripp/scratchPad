6e1faab318c08372852a58fd42386ff3e91b0c5f
calebtripp <calebtripp@outlook.com>
2022-09-15 16:26:00 -0400

    todays notes

---
+++ loops/WhileLoops/WhileLoops/Program.cs
@@ -0,0 +1,94 @@
+﻿
+//Console.WriteLine("guess my favorite number");
+//var answer = Console.ReadLine();
+
+//var favNum = 2;
+
+//while (true)
+//{
+//    if (favNum)
+//    Console.WriteLine("Too High!");
+//    Console.WriteLine("Keep Trying");
+//    Console.ReadLine();
+
+//    break;
+//}
+
+
+
+
+
+//// do while
+/////////////////////////////////////////////
+//string password = "TrueCoders";
+
+
+//Console.WriteLine("Please enter your password");
+//string userInput = Console.ReadLine();
+
+//do
+//{
+//    Console.WriteLine("please try again");
+//    Console.WriteLine("enter your password");
+//    userInput = Console.ReadLine();
+//} while (userInput != password);
+
+
+/////////////////////add "great job, you got it! for the guess favnum
+/////
+
+/////////////////////////////////////////////////////
+
+
+///////////////////////////////////
+
+//int y = 0;
+//do
+//{
+//    Console.WriteLine(y);
+//} while (y>0);
+
+/////////////////////////////////////
+///
+
+//for loops
+
+//int[] myArray = { 10, 20, 30, 40, 50 };
+
+//for (int i = 0; i < myArray.Length; i++)
+//{
+//    Console.WriteLine(myArray[i]);
+//}
+
+
+//////////////////////////
+///
+
+//for each loop -- used to iterate (loop) arrays or lists.
+/////////////////////////////////////////////////////////
+using System.Collections.Generic;
+///
+///
+var videoGames = new List<string>()
+{
+    "Elden Ring",
+    "Quarry",
+    "God of War",
+    "Mortal Kombat",
+    "Destiny 2",
+    "Pokemon Artemis"
+
+};
+
+foreach (var game in videoGames)
+{
+    Console.WriteLine(game);
+}
+
+for (int i = 0; i < length; i++)
+{
+    List.add($"i");
+    Console.WriteLine();
+}
+
+
--- scratchPad/scratchPad/Program.cs
+++ scratchPad/scratchPad/Program.cs
@@ -1,14 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
 
-//interpolation
-var circles = 10;
-Console.WriteLine($"run around in circles {circles} times");
+////interpolation
+//var circles = 10;
+//Console.WriteLine($"run around in circles {circles} times");
 
-//add a line (hit enter or + )
-var circles2= 11;
-var circles3= 12;
-Console.WriteLine($"run around in circles {circles2} times,\n" +
-          $"run around in circles {circles2} times");
+////add a line (hit enter or + )
+//var circles2= 11;
+//var circles3= 12;
+//Console.WriteLine($"run around in circles {circles2} times,\n" +
+//          $"run around in circles {circles2} times");
 
+//var myInt = 14;
+//if (myInt > 10 && "Hey" == "Hey") 
+//{
+//    Console.WriteLine();
+
+//}
+
+
+var myInt = 14;
+if (myInt > 10 && "Hey" == "Hey") { Console.WriteLine("It works it works, he's alive!"); }
 
